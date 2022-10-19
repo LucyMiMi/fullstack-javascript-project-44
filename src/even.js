f4284e258d28e3945f707fde411fb5320074e035
LucyMiMi <lucymur.dev@gmail.com>
2022-10-19 09:38:59 +0300

    Logic brain-even

--- src/even.js
+++ src/even.js
@@ -1,27 +1,36 @@
 import readlineSync from 'readline-sync';
 
 export default (input_value) => {
-        const number_variant = (random_number, answer) => {  
-            const even_correct = random_number % 2 === 0 && answer === 'yes';    
-            const odd_correct =  random_number % 2 !== 0 && answer === 'no';    
-            const correct = 'Correct!';
-            const grats = `Congratulations, ${name}`;
-            const incorrect_yes = '\'yes\'is wrong answer;(. Correct answer was \'no\'';
-            const incorrect_no = '\'no\'is wrong answer;(. Correct answer was \'yes\'';
-            const advice_to_try = 'Let\'s try again, Bill!';
-            for (let i = 0; i < 3; i += 1) {     
-                const even = even_correct ? correct : incorrect_no;
-                const odd = odd_correct ? correct : incorrect_yes;
-                if(incorrect_no || incorrect_yes) return advice_to_try;
-                return grats;
-            }
-        }
-
-    const random_number = Math.random(10);
-    const space = ' ';
+    const space = ' ';   
     const offer = 'Answer "yes" if the number is even, otherwise answer "no".';
+    const correct = 'Correct!';            
+    const incorrect_even_yes = '\'yes\'is wrong answer;(. Correct answer was \'no\'';
+    const incorrect_even_no = '\'no\'is wrong answer;(. Correct answer was \'yes\'';
+    
     const name = readlineSync.question(`May I have your name?${space}`);
     console.log(`Hello, ${name} \n ${offer}`);
-    
+
+    const grats = `Congratulations, ${name}`;
+    const advice_to_try = `Let\'s try again, ${name}`;
+    let count = 0;
+    for (let i = 0; count < 3; i += 1) {
+        let random_number = Math.floor(Math.random() * 10);
+        const answer = readlineSync.question(`${random_number}\n`);
+        if (random_number % 2 === 0 && answer === 'yes') {
+            console.log(`${correct}`);
+            count += 1;
+        } else if (random_number % 2 === 0 && answer === 'no') {
+            console.log(`${incorrect_even_no}\n ${advice_to_try}`);
+            break;
+        }
+        if (random_number % 2 !== 0 && answer === 'no') {
+            console.log(`${correct}`);
+            count += 1;
+        } else if (random_number % 2 !== 0 && answer === 'yes') {
+            console.log(`${incorrect_even_yes}\n ${advice_to_try}`);
+            break;
+        }
+        if(count === 3) console.log(grats);
+    } 
 
 };
