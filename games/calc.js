b1821f21e3ac0bd36c25b730bd05ee56f2d7a63d
LucyMiMi <lucymur.dev@gmail.com>
2022-10-20 23:22:31 +0300

    realization calc.js

--- games/calc.js
+++ games/calc.js
@@ -8,12 +8,27 @@ export default (input_value) => {
     const correct_answer = [];
     
     for(let i = 0; i < 3; i +=1) {
-        let operator = random_operator;
-        let num = randomNumber(1, 50);
-        expression.push(num);
-        let answer = num % 2 === 0 ? `${answer_yes}` : `${answer_no}`;
-        correct_answer.push(answer);
+        let num_first = randomNumber(1, 30);
+        let num_two = randomNumber(1, 30);
+        let operator = random_operator();
+        if (operator === '+') {
+           answer =  num_first + num_two;
+           const string_expression = `${num_first} ${operator} ${num_two}`
+           expression.push(string_expression);
+           correct_answer.push(answer);
+        }
+        if (operator === '-') {
+            answer =  num_first - num_two;
+            const string_expression = `${num_first} ${operator} ${num_two}`
+            expression.push(string_expression);
+            correct_answer.push(answer);
+        }
+        if (operator === '*') {
+            answer =  num_first * num_two;
+            const string_expression = `${num_first} ${operator} ${num_two}`
+            expression.push(string_expression);
+            correct_answer.push(answer);
+         }      
     }
-
     game_base(definition, expression, correct_answer);
 };
