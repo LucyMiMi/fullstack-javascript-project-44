6b869cd1d6f6e0089c17e2acdde1aee66eaabbb7
LucyMiMi <lucymur.dev@gmail.com>
2022-10-21 20:53:16 +0300

    for test

--- util/randomOperator.js
+++
@@ -1,5 +0,0 @@
-export default () => {
-    const operator_col = ['+', '-', '*'];
-    let randomize_operator = Math.floor(Math.random() * operator_col.length)
-    return operator_col[randomize_operator];
-};
