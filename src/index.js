5525bb0e5e47d707681c4146d58f469ee7c60ca0
LucyMiMi <lucymur.dev@gmail.com>
2022-10-21 10:12:49 +0300

    fixed output

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 import readlineSync from 'readline-sync';
 
-    export default (definition, expression, correct_answer) => {       
+    export default (definition, expression, correct_answer) => {
         const name = readlineSync.question(`May I have your name? `);
         console.log(`Hello, ${name}\n${definition}`);
 
         const correct_answer_message = 'Correct!';
         const grats = `Congratulations, ${name}`;
 
-        for (let round = 0; round < 3; round += 1) {  
-                const round_question = expression[round];           
-                const answer = readlineSync.question(`Question: ${round_question}\nYour answer: `);              //console.log(round_max);
+        for (let round = 0; round < 3; round += 1) {
+                const round_question = expression[round];
+                console.log(`Question: ${round_question}`)
+                const answer = readlineSync.question(`Your answer: `);
                 if (answer !== correct_answer[round]) {
                     console.log(`'${answer}' is wrong $;(. Correct answer was '${correct_answer[round]}' \n Let's try again, ${name}`);
-                    break;         
+                    break;
                 }
                 console.log(`${correct_answer_message}`);
                 if(round === 2) console.log(`${grats}`);
-            }            
+            }   
 };
