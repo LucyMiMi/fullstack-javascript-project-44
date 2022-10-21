2f6484b50bf69ed1710a839ddc26834e9f3106dc
LucyMiMi <lucymur.dev@gmail.com>
2022-10-21 06:17:07 +0300

    fixed bugs

--- util/nod.js
+++ util/nod.js
@@ -1,4 +1,4 @@
-const nod = (num_first, num_two) => {
+export default (num_first, num_two) => {
     if (num_two !== 0) {
       const num_temp = num_first % num_two;
       return nod(num_two, num_temp);
